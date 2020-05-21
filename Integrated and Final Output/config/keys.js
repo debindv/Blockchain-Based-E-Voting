aa5feea27e975f1dd5b91b452477d5ea4acd13ca
debindv <debinptpm@gmail.com>
2020-05-21 15:08:08 +0530

    Added integration and deploy folders

--- Integrated and Final Output/config/keys.js
+++
@@ -1,5 +0,0 @@
-dbPassword = 'mongodb+srv://Asher:evoting123@testcluster-yczvx.mongodb.net/test?retryWrites=true&w=majority';
-
-module.exports = {
-    mongoURI: dbPassword
-};
--- Integrated and Final Output/migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-const Election = artifacts.require("./Election.sol");
-//artifacts respresents the contract abstarction that is specific to truffle
-module.exports = function(deployer) {
-  deployer.deploy(Election);
-};
